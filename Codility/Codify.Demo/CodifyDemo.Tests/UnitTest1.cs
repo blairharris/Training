e5783d8f158e69c7213997fcbee0edd21035174d
Blair Harris <jonathan.blair.harris@gmail.com>
2016-02-04 20:38:10 +0000

    Added for first time

---
+++ Codility/Codify.Demo/CodifyDemo.Tests/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodilityDemoLib;
+using FluentAssertions;
+
+namespace CodilityDemo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            int[] array = {-1, 3, -4, 5, 1, -6, 2, 1};
+
+            int result = Demo.Solution(array);
+            result.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void ZeroOnly()
+        {
+            int[] array = { 0 };
+
+            int result = Demo.Solution(array);
+            result.Should().Be(0);
+        }
+        [TestMethod]
+        public void TwoZeros()
+        {
+            int[] array = { 0, 0 };
+
+            int result = Demo.Solution(array);
+            result.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void IntMinValue()
+        {
+            int[] array = {0, int.MinValue, int.MinValue };
+
+            int result = Demo.Solution(array);
+            result.Should().Be(-1);
+        }
+        [TestMethod]
+        public void IntMaxValue()
+        {
+            int[] array = { 0, int.MaxValue, int.MaxValue };
+
+            int result = Demo.Solution(array);
+            result.Should().Be(-1);
+        }
+        [TestMethod]
+        public void IntMaxValueButTrueEqPoint1()
+        {
+            int[] array = { int.MaxValue, 0, int.MaxValue };
+
+            int result = Demo.Solution(array);
+            result.Should().Be(1);
+        }
+        [TestMethod]
+        public void IntMaxValueButTrueEqPoint2()
+        {
+            int[] array = { -1, int.MaxValue, 0, -1, int.MaxValue };
+
+            int result = Demo.Solution(array);
+            result.Should().Be(2);
+        }
+        [TestMethod]
+        public void IntMaxValueButTrueEqPoint3_Overflow()
+        {
+            int[] array = { 1, int.MaxValue, 0, 1, int.MaxValue };
+
+            int result = Demo.Solution(array);
+            result.Should().Be(-1);
+        }
+    }
+}
---
+++ Codility/Codify.Demo/CodifyDemoLib/Demo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityDemoLib
+{
+    public class Demo
+    {
+        public static int Solution(int[] a)
+        {
+            var equilibiumIndex = -1;
+
+            for (var p = 0; p < a.Length; p++)
+            {
+                try
+                {
+                    var lowerSum = Demo.LowerSum(a, p);
+                    var upperSum = Demo.UpperSum(a, p);
+
+                    if (lowerSum != upperSum) continue;
+                    equilibiumIndex = p;
+                    break;
+                }
+                catch (OverflowException)
+                {
+                }
+            }
+
+            return equilibiumIndex;
+        }
+
+        public static int LowerSum(int[] a, int p)
+        {
+            var sum = 0;
+
+            for (var index = 0; index < p; index++)
+            {
+                sum = checked(sum + a[index]);
+            }
+
+            return sum;
+        }
+
+        public static int UpperSum(int[] a, int p)
+        {
+            var sum = 0;
+
+            for (var index = p + 1; index < a.Length; index++)
+                sum = checked(sum + a[index]);
+
+            return sum;
+        }
+    }
+}
